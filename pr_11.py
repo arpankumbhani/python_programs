b3411e10a68dbb5f14a46833be980590ba0c6738
arpankumbhani <arpankumbhani@gmail.com>
2023-04-27 00:51:48 +0530

    add

---
+++ pr_11.py
@@ -0,0 +1,3 @@
+#write a program that  takes a list of numbers(for example,a=[5,10,15,20,25 ])and makes a new  list of only the first elements of the given list.for practice,write this code inside a function
+a=[5,10,15,20,25]
+print(a[0],a[-1])

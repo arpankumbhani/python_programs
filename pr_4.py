1febcded58279b43b8f719f189a7998a52c4cfc3
arpankumbhani <arpankumbhani@gmail.com>
2023-04-26 23:11:57 +0530

    program for done

--- pr_4.py
+++ pr_4.py
@@ -1,1 +1,8 @@
-print('hello')
+#take a list, say for example this one:a[1,1,2,3,5,8,13,21,34,55,89],and write a program that peints out all the elements on the list that are less than 5.
+#[1]
+a=(1,1,2,3,5,8,13,21,34,55,89)
+print(a)
+for x in a:
+    if x<5:
+        print(x)
+
